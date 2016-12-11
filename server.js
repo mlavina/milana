bc5d4643488a21bd6bdd9bb51fa4008a1ed3c531
mlavina <michael.lavina@gmail.com>
2016-12-11 15:47:33 -0500

    chore(node): updated node version to node 6

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import * as express from 'express';
 let app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname +'/dist'));
