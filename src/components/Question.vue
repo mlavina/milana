8fb474718673bcacf0c1bc4f233189619bda6fab
mlavina <michael.lavina@gmail.com>
2016-12-11 15:10:37 -0500

    chore(init): initial commit

---
+++ src/components/Question.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="question-text"><a class='youtube' :href="">Question {{ questionNum }} </a></div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+    }
+</script>
