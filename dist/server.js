336834322b8602faa7d713f44c4627697dc1ddae
Michael A. Lavina <michael.lavina@factset.com>
2016-12-11 23:14:33 -0500

    fix(package): fix attempt at static files in root directory

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var path = require('path');
 
 var app = express();
 
-var CWD = process.cwd();
-
-var DIST = path.join(CWD, 'dist');
-
-app.use('/', express.static(DIST));
+app.use('/', express.static(__dirname));
 
 var port = Number(process.env.PORT || 8000);
 app.listen(port, function() {
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
 
 var app = express();
 
-app.use(serveStatic(path.join(__dirname +'dist')));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('Server started on ' + port);
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("listening on " + port);
+});
